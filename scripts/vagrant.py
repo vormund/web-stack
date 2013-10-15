5e3e455c15b9fe5a2357ee4b7f663a7c4d250f4e
Joshua Henn <smysnk@gmail.com>
2013-10-15 11:26:59 -0600

    vagrant python script dev

--- scripts/vagrant.py
+++ scripts/vagrant.py
@@ -4,6 +4,18 @@ import argparse, subprocess, re, sys
 VERSION="0.1";
 
 parser = argparse.ArgumentParser(description='''Vagrant Operation Helper''',)
-parser.add_argument('--up', '--down' type=str, help="Setup or Teardown")
+subparsers = parser.add_subparsers(help='Setup / Teardown help')
+
+#group = parser.add_mutually_exclusive_group()
+#group.add_argument('--up', action='store_true', help="Setup camp")
+#group.add_argument('--down', action='store_true', help="Teardown camp")
+
+upParser = subparsers.add_parser('up', help='Setup help')
+upParser.add_argument('docker', type=int, nargs='*', help='bar help')
+upParser.add_argument('bar', type=int, help='bar help')
+
+downParser = subparsers.add_parser('down', help='Teardown help')
+downParser.add_argument('bar', type=int, help='bar help')
+
 parser.add_argument('--version', action='version', version=VERSION, help="Return version of script")
 args = parser.parse_args()
