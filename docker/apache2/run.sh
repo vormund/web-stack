f40c0cf5c792499048d3495e9fceb02d9b51c365
Joshua Bellamy-Henn <smysnk@gmail.com>
2013-12-06 00:50:51 -0700

    fix(apache2): Was not exposing port 8080

--- docker/apache2/run.sh
+++ docker/apache2/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Connect together all the docker containers
 
-docker run -d -i -t -dns 172.17.42.1 -dns 8.8.8.8 -dns 8.8.4.4 -p 2200:22 -v /vagrant/docker/apache2/var/www:/var/www apache2
+docker run -d -i -t -dns 172.17.42.1 -dns 8.8.8.8 -dns 8.8.4.4 -p 2200:22 -p 8080:80 -v /vagrant/docker/apache2/var/www:/var/www apache2
--- webstack.py
+++ webstack.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import dockyard
 
 cli = dockyard.Cli()
